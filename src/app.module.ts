a11589a44add323a91dd4f7f6fca968fb6bc0f37
WinZawOoDev <winzawoo.dev@gmail.com>
2024-05-01 22:34:16 +0630

    setup user post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroesModule } from './heroes/heroes.module';
+import { PostService } from './post.service';
+import { UserService } from './user.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [HeroesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostService, UserService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
