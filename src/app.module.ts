13bc902cff2b57819f043b7a9e401909ec9a4826
WinZawOoDev <winzawoo.dev@gmail.com>
2024-05-01 10:08:56 +0630

    fix: remove hero

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HeroModule } from './hero/hero.module';
 
 @Module({
-  imports: [HeroModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
